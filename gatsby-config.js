330a90a3fb47cbef42599b149dabfe90fe62e66a
Ethan Cohen <ethanvcohen@gmail.com>
2022-06-20 13:27:11 -0400

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Ethan Cohen`,
-    subtitle: `sr. software engineer @twitter`,
+    subtitle: `staff software engineer @twitter`,
     description: ``,
     author: `@beefan`,
   },
